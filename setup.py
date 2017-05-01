b3e13e4643ea6656158d731d114d1db9a78ac786
John Wittrock <john@sidewalklabs.com>
2017-05-01 17:40:19 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='ssd-tensorflow',
+    version=0.0.1,
+    description='TensorFlow implementation of SSD networks',
+    long_description=open('README.md', 'rb').read().decode('utf-8')
+    author='Paul B.'
+    author_email='paulb@example.com'
+    url='https://github.com/balancap/SSD-Tensorflow',
+    packages=['nets', 'preprocessing', 'notebooks'],
+    install_requires=[
+        'tensorflow>=0.12',
+        'numpy>=1.11.0',
+        'matplotlib>=2.2.0',
+    ],
+    license='',
+)
