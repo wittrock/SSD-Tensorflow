92be5db69426ae2f318b8726e98f91c19d7b4f46
John Wittrock <john@sidewalklabs.com>
2017-05-01 17:42:31 -0400

    commas

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='ssd-tensorflow',
     version='0.0.1',
     description='TensorFlow implementation of SSD networks',
-    long_description=open('README.md', 'rb').read().decode('utf-8')
-    author='Paul B.'
-    author_email='paulb@example.com'
+    long_description=open('README.md', 'rb').read().decode('utf-8'),
+    author='Paul B.',
+    author_email='paulb@example.com',
     url='https://github.com/balancap/SSD-Tensorflow',
     packages=['nets', 'preprocessing', 'notebooks'],
     install_requires=[
